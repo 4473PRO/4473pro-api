8b724e382425f3a4f0c866ecf4e733b8d068cb44
4473PRO <info@4473pro.com>
2026-03-13 14:12:18 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2620,7 +2620,7 @@ def get_leaderboard_data():
 
     try:
         from datetime import datetime, timezone, timedelta
-        since = (datetime.now(timezone.utc) - timedelta(days=days)).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+        since = (datetime.now(timezone.utc) - timedelta(days=days)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
         r = requests.get(
             f"{SB_URL}/rest/v1/audit_history?profile_id=eq.{owner_id}"
